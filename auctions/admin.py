682d80346418954c0c5184ccefef99b4d30c153a
fabimass <massotto.fabian@gmail.com>
2023-02-14 19:58:04 -0300

    customizing the admin page for the listings

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Listing, Bid, Comment, User
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "category", "listed_by", "is_active")
+    filter_horizontal = ("users_watching",)
+
 # Register your models here.
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
 admin.site.register(Bid)
 admin.site.register(Comment)
 admin.site.register(User)
